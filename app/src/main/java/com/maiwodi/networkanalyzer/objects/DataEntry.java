7a7b515e7fa364cf891e264808ed272ec6e975d6
Bryan Wodi <talk2kamp@gmail.com>
2019-04-06 22:59:02 -0500

    collecting internet connection speed and removed toString method

--- app/src/main/java/com/maiwodi/networkanalyzer/objects/DataEntry.java
+++ app/src/main/java/com/maiwodi/networkanalyzer/objects/DataEntry.java
@@ -3,25 +3,11 @@ package com.maiwodi.networkanalyzer.objects;
 public class DataEntry {
     private String timeStamp;
     private int rssiValue;
+    private int speedInMbps;
 
-    public DataEntry(String timeStamp, int rssiValue) {
+    public DataEntry(String timeStamp, int rssiValue, int speedInMbps) {
         this.timeStamp = timeStamp;
         this.rssiValue = rssiValue;
-    }
-
-    public String getTimeStamp() {
-        return timeStamp;
-    }
-
-    public int getRssiValue() {
-        return rssiValue;
-    }
-
-    @Override
-    public String toString() {
-        return "DataEntry{" +
-                "timeStamp='" + timeStamp + '\'' +
-                ", rssiValue=" + rssiValue +
-                '}';
+        this.speedInMbps = speedInMbps;
     }
 }
